3506d7f94955137f0af4738ee67b11fdfbb0aa04
Akyless <akyless@yandex.ru>
2018-12-29 23:51:27 +0300

    Сделала задание 01  https://github.com/Akyless/LearningJava/issues/1

---
+++ src/main/java/com/akyless/learning/Exercise1.java
@@ -0,0 +1,21 @@
+package com.akyless.learning;
+// Даны две числовые переменные, задача — написать программу, меняющую местами эти переменные
+public class Exercise1 {
+    public static void main(String[] args){
+        int a = 17;
+        int b = 32;
+
+        System.out.println("a = " +a);
+        System.out.println("b = " +b);
+        System.out.println("#################");
+
+        // code
+        int c = a;
+        a=b;
+        b=c;
+
+        System.out.println("a = " +a);
+        System.out.println("b = " +b);
+    }
+}
+
