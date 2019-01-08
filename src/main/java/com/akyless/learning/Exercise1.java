d6293ed18c1fc2d7b27e107da9161f6e9c779ee4
akyless <akyless@yandex.ru>
2019-01-08 19:53:20 +0300

    Отформатировала текст 1 задания  https://github.com/Akyless/LearningJava/issues/1

--- src/main/java/com/akyless/learning/Exercise1.java
+++ src/main/java/com/akyless/learning/Exercise1.java
@@ -1,21 +1,22 @@
 package com.akyless.learning;
+
 // Даны две числовые переменные, задача — написать программу, меняющую местами эти переменные
 public class Exercise1 {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         int a = 17;
         int b = 32;
 
-        System.out.println("a = " +a);
-        System.out.println("b = " +b);
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
         System.out.println("#################");
 
         // code
         int c = a;
-        a=b;
-        b=c;
+        a = b;
+        b = c;
 
-        System.out.println("a = " +a);
-        System.out.println("b = " +b);
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
     }
 }
 
