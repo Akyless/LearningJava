50d36dd9c49f065e5e400cd81f0644e09660ae17
akyless <akyless@yandex.ru>
2019-01-11 00:34:20 +0300

    Работала с переменными.

---
+++ src/main/java/com/akyless/learning/Exercise2.java
@@ -0,0 +1,34 @@
+package com.akyless.learning;
+
+import java.util.Scanner;
+
+public class Exercise2 {
+
+ public static void main (String[] args) {
+     int a = 0;
+     boolean b = false;
+     char s = 'A';
+     float z = 0;
+     double w = 3.14;
+     byte q;
+     long e = 17;
+     short u = 1;
+     String d= "Война и мир";
+     String x = "Жениш,Алмаз Акылес~!";
+     System.out.println(x);
+
+     String ppp;
+     ppp = new Scanner(System.in).next();
+     System.out.println(ppp);
+
+     int f;
+     f = new Scanner(System.in).nextInt();
+     f*=2;
+     System.out.println(f);
+
+ }
+}
+
+
+
+
