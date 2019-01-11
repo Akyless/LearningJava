f027bf54587e7bc30fd6a0b6e94b9db8d8260070
akyless <akyless@yandex.ru>
2019-01-11 17:48:53 +0300

    Внесла изменения в задание 21 (max and min)

--- src/main/java/com/akyless/learning/Exercise3.java
+++ src/main/java/com/akyless/learning/Exercise3.java
@@ -6,29 +6,61 @@ public class Exercise3 {
 
 public static void main(String[] args) {
 
- String a = "Выведи мне значение переменной 'q'";
- System.out.println(a);
- int q;
- q = new Scanner(System.in).nextInt();
- System.out.println(q);
+ System.out.println("Please input q:");
+ int q = new Scanner(System.in).nextInt();
+ System.out.println("Please input w:");
+ int w = new Scanner(System.in).nextInt();
+ System.out.println("Please input e:");
+ int e = new Scanner(System.in).nextInt();
 
- String s = "Выведи мне значение перменной 'w'";
- System.out.println(s);
- int w;
- w = new Scanner(System.in).nextInt();
- System.out.println(w);
+ System.out.println("q=" +q);
+ System.out.println("w=" +w);
+ System.out.println("e=" +e);
 
- String d = "Выведи мне значение переменной 'e'";
- System.out.println(d);
- int e;
- e = new Scanner(System.in).nextInt();
- System.out.println(e);
-
+ System.out.println("max=" + Math.max(q,e));
+ System.out.println("min=" + Math.min(q,w));
 
     if((q>w)&(q>e)&(w>e)) {
      System.out.println("max = q; min = e");
- } else{
+    } else if(q<e) {
      System.out.println("max = e, min = q");
- }
+    }
+
+    if((q>w)&(q>e)&(w<e)) {
+        System.out.println("max = q; min = w");
+    } else if(q<w) {
+        System.out.println("max = w; min = q");
+    }
+
+    if((q>w)&(q<e)&(e>w)) {
+        System.out.println("max = e; min = w");
+    } else if(q>e) {
+        System.out.println("max = w; min = e");
+    }
+
+    if((q>w)&(q<e)&(e<w)) {
+        System.out.println("max = e; min = q");
+    } else if(e>w) {
+        System.out.println("max = q; min = e");
+    }
+
+    if((q<w)&(w>e)&(e>q)) {
+        System.out.println("max = w; min = q");
+    } else if(w<e) {
+        System.out.println("max = q; min = w");
+    }
+
+    if((q<w)&(w>e)&(e<q)) {
+        System.out.println("max = w; min = e");
+    } else if(q>w) {
+        System.out.println("max = e; min = w");
+    }
+
+    if((q<w)&(w<e)&(q<e)) {
+        System.out.println("max = e; min = q");
+    } else if(q>w) {
+        System.out.println("max = q; min = e");
+    }
+
 }
 }
