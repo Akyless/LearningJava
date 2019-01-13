e67164729f85f2235d8c89eede118eb596f2a567
akyless <akyless@yandex.ru>
2019-01-13 17:44:25 +0300

    Исправила значение char (с а на b)

--- src/main/java/com/akyless/learning/Exercise10.java
+++ src/main/java/com/akyless/learning/Exercise10.java
@@ -9,12 +9,11 @@ public class Exercise10 {
         System.out.println("Please input a");
         int a = scanner.nextInt();
         char b = (char) a;
-        System.out.println(b);
 
         if (a > 64 & a < 91) {
-            System.out.println("char a = " + a);
+            System.out.println("char b = " + b);
         } else if (a > 96 & a < 123) {
-            System.out.println("char a = " + a);
+            System.out.println("char b = " + b);
         } else {
             System.out.println("Это не буква Англиского алфавита!");
         }
