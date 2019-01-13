ca4214c4fc968238d719d230a5dba4ab8de880c2
akyless <akyless@yandex.ru>
2019-01-13 14:21:13 +0300

    Отформатировала файл текста в задании 22(мат фун-ция)

--- src/main/java/com/akyless/learning/Exercise7.java
+++ src/main/java/com/akyless/learning/Exercise7.java
@@ -7,12 +7,12 @@ public class Exercise7 {
         System.out.println("Please input x: ");
         int x = new Scanner(System.in).nextInt();
 
-        if (x>0) {
-            System.out.println("y = " + (2*x - 10));
+        if (x > 0) {
+            System.out.println("y = " + (2 * x - 10));
         } else if (x == 0) {
             System.out.println("y = 0");
-        } else if (x<0) {
-            System.out.println("y = " + (2*(Math.abs(x)) - 1));
+        } else {
+            System.out.println("y = " + (2 * (Math.abs(x)) - 1));
         }
 
     }
