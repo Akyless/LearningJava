1f873a8eaa2ee2fc307ac0efef297782ed344d61
akyless <akyless@yandex.ru>
2019-01-15 18:14:50 +0300

    Cделала задание 29(таблица умножения)

---
+++ src/main/java/com/Sprint2/ExerciseS29.java
@@ -0,0 +1,13 @@
+package com.Sprint2;
+
+public class ExerciseS29 {
+
+    public static void main(String[] args) {
+        for (int a = 1; a <= 9; a++) {
+            for (int b = 1; b <= 9; b++) {
+                System.out.println(a + "*" + b + "=" + (a*b));
+            }
+            System.out.println();
+        }
+    }
+}
