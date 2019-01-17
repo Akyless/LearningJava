c1febfbc60c7489e7e2e74750c8e1508a348af3c
akyless <akyless@yandex.ru>
2019-01-17 22:29:47 +0300

    Cделала задание 28(наибольшая цифра в числе)

---
+++ src/main/java/com/Sprint2/ExerciseS28.java
@@ -0,0 +1,24 @@
+package com.Sprint2;
+
+import java.util.Scanner;
+
+public class ExerciseS28 {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please input a");
+        int a = scanner.nextInt();
+        int b = (Math.abs(a));
+        int max = 0;
+        if (a != 0) {
+            while (a > 0) {
+                if (a % 10 > max) max = (int) (a % 10);
+                a /= 10;
+            }
+            System.out.println("Наибольшей цифрой является  " + max);
+        } else {
+            System.out.println("Заданное число не является натуральным!");
+            System.out.println(" Try once more!!");
+        }
+    }
+}
