051e84a70704c6fff75e5f38cd7573d53c278277
akyless <akyless@yandex.ru>
2019-01-21 21:26:34 +0300

    Cделала задание 38 (2 попыка -сумма четных+ элементов)

--- src/main/java/com/Sprint2/ExerciseS38.java
+++ src/main/java/com/Sprint2/ExerciseS38.java
@@ -5,24 +5,17 @@ import java.util.Random;
 
 public class ExerciseS38 {
     public static void main(String[] args) {
-
-        int array[] = new int[10];
-        boolean positive = true;
+        int[] array = new int[10];
         int ch = 0;
-
         for (int i = 0; i < array.length; i++) {
-            array[i] = new Random().nextInt(40) + 10;
+            array[i] = new Random().nextInt(100) - 50;
             if (array[i] > 0) {
                 if (array[i] % 2 == 0) {
                     ch += array[i];
-                } else {
-                    System.out.println();
                 }
-            } else {
-                System.out.print("Это отрицательное число!");
             }
         }
         System.out.println(Arrays.toString(array));
-        System.out.println(ch);
+        System.out.println("Сумма четных положительных элементов - " + ch);
     }
 }
